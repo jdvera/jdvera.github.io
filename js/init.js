10960fb632b4d120bd9d7a3efe71bfe682d8ec8c
James Vera <jdvera2015@gmail.com>
2019-08-20 17:09:15 -0500

    checking if eager loading strat is working

--- js/init.js
+++ js/init.js
@@ -10,5 +10,17 @@
       $(this).attr("src", $(this).data("still"));
     });
 
+    Image1 = new Image(1600, 900);
+    Image1.src = "./images/coffee.gif";
+
+    Image2 = new Image(1600, 900);
+    Image2.src = "./images/deck.gif";
+
+    Image3 = new Image(1600, 900);
+    Image3.src = "./images/spell.gif";
+
+    Image4 = new Image(1600, 900);
+    Image4.src = "./images/viper.gif";
+
   });
 })(jQuery);
